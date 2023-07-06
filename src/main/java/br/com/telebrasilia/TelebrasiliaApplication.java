b7e43509425a6331fc3079d6755ee037b3336d19
Romerito <romerito.alencar@gmail.com>
2023-07-06 20:42:12 -0300

    first commit

---
+++ src/main/java/br/com/telebrasilia/TelebrasiliaApplication.java
@@ -0,0 +1,13 @@
+package br.com.telebrasilia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelebrasiliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelebrasiliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/telebrasilia/TelebrasiliaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.telebrasilia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelebrasiliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
