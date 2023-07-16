e69bae83007121619f5603c1f9cadf628d49f4d4
Romerito <romerito.alencar@gmail.com>
2023-07-16 10:05:25 -0300

    upload e download de files

--- src/main/java/br/com/telebrasilia/TelebrasiliaApplication.java
+++ src/main/java/br/com/telebrasilia/TelebrasiliaApplication.java
@@ -1,8 +1,13 @@
 package br.com.telebrasilia;
 
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import br.com.telebrasilia.upload.FilesStorageService;
 
 /**
  * @author  Romerito Alencar
@@ -10,10 +15,19 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
-public class TelebrasiliaApplication {
+public class TelebrasiliaApplication implements CommandLineRunner {
+
+	@Resource
+	FilesStorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TelebrasiliaApplication.class, args);
-	}
+	}	
 
+	
+ @Override
+  public void run(String... arg) throws Exception {
+    //storageService.deleteAll();
+    storageService.init();
+  }
 }
